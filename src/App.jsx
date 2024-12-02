d3287c5ee51be11402288f18994f0b7dba4353d1
Nukala SaiCharan <charan.nukala12@gmail.com>
2024-12-02 14:20:29 +0530

    Final Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Dashboard from './components/Dashboard';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signin from './components/Signin';
+
+const App = () => {
+  
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signin/>}/>
+        <Route path="/dashboard" element={<Dashboard/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+
+
